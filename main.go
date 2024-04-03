6880b2e0d1b065ecb48792d517319670aaca0c1b
DanuhPutra <ngurahdanuhputra@gmail.com>
2024-04-04 00:30:03 +0800

    Edit by Danuh

--- main.go
+++ main.go
@@ -2,17 +2,61 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/MasterDimmy/go-cls"
 )
 
+// public variable
+var FilePath string
+
+// private function
+func importFileCsv() {
+	// ini case CLI saat dijalankan tanpa package flag
+	cls.CLS()
+	var err error
+	
+	fmt.Print("Masukkan path lokasi file CSV: ")
+	fmt.Scanln(&FilePath)
+
+	FilePath, err = filepath.Abs(FilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\n\n\n======================================")
+	fmt.Println("Tekan 'Enter' untuk melanjutkan...")
+	fmt.Println("\n======================================")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
 
 func main(){
-	cls.CLS()
-	fmt.Println("testing project")
+	importFileCsv()
+	// ngebukak file csv nya
+	file, err := os.Open(FilePath)
+	if err != nil {
+		// case error nya
+		fmt.Println("Error:", err)
+		return
+	}
+	// close file csv
+	defer file.Close() 
 
+	// variabel yg isinya baru ngebaca file csv
+	reader := csv.NewReader(file)
+
+	// ni ngebaca semuanya 
+	records, err := reader.ReadAll()
+	if err != nil {
+		// case error
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(records)
 
 	fmt.Println("\n======================================")
 	fmt.Println("Tekan 'Enter' untuk melanjutkan...")
