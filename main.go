492d20ea20f1762ecb93a2cbe116fec3aa4f800b
Pande17 <pandedwiwjy@gmail.com>
2024-04-09 23:05:09 +0800

    edit by pande

--- main.go
+++ main.go
@@ -114,6 +114,7 @@ func main() {
 
         // itungan lambat bar
 		time.Sleep(40 * time.Millisecond)
+		cls.CLS()
         bar.Add(1)
     }
 
--- model/base_data_penduduk.go
+++ model/base_data_penduduk.go
@@ -5,12 +5,12 @@ import (
 )
 
 type BaseData struct {
-	id         string    `json:"_id"`
-	name       string    `json:"name"`
-	email      string    `json:"email"`
-	phone      int       `json:"phone"`
-	address    string    `json:"address"`
-	city       string    `json:"city"`
-	postalcode int       `json:"postal_code"`
+	ID         string    `json:"_id"`
+	Name       string    `json:"name"`
+	Email      string    `json:"email"`
+	Phone      int       `json:"phone"`
+	Addres     string    `json:"address"`
+	City       string    `json:"city"`
+	Postalcode int       `json:"postal_code"`
 	Tanggal    time.Time `json:"tanggal"`
 }
